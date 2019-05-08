418e5f1f7721b8d9875d72b2b932fcd8e3b8e52a
Northcoders4 <northcoders4@Northcoders4s-MacBook-Pro.local>
2019-05-08 16:23:16 +0100

    seeded

--- db/data/dev-data/users.js
+++ db/data/dev-data/users.js
@@ -1,49 +1,49 @@
 module.exports = [
   {
     email: 'jonny.bravo@arup.com',
-    firstName: 'jonny',
+    first_name: 'jonny',
     lastName: 'bravo',
     password: 'jonny123'
   },
   {
     email: 'tish.richardson@arup.com',
-    firstName: 'tish',
+    first_name: 'tish',
     lastName: 'richardson',
     password: 'tish123'
   },
   {
     email: 'ben.adey@arup.com',
-    firstName: 'ben',
+    first_name: 'ben',
     lastName: 'adey-johnson',
     password: 'ben123'
   },
   {
     email: 'martyn.tattersal@arup.com',
-    firstName: 'martyn',
+    first_name: 'martyn',
     lastName: 'tattersal',
     password: 'martyn123'
   },
   {
     email: 'jim.bob@arup.com',
-    firstName: 'jim',
+    first_name: 'jim',
     lastName: 'bob',
     password: 'jim123'
   },
   {
     email: 'james.smith@arup.com',
-    firstName: 'james',
+    first_name: 'james',
     lastName: 'smith',
     password: 'james123'
   },
   {
     email: 'anne.smith@arup.com',
-    firstName: 'anne',
+    first_name: 'anne',
     lastName: 'smith',
     password: 'anne123'
   },
   {
     email: 'jack.white@arup.com',
-    firstName: 'jack',
+    first_name: 'jack',
     lastName: 'white',
     password: 'jack123'
   }
--- db/migrations/20190508115935_answers_options.js
+++ db/migrations/20190508115935_answers_options.js
@@ -1,7 +1,7 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('answers_options', riskLevelTable => {
     riskLevelTable.increments('answers_options_id').primary();
-    riskLevelTable.string('answer', 1);
+    riskLevelTable.string('answer', 4);
   });
 };
 
