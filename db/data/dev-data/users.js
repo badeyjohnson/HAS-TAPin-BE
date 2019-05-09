41a1dfcb2bd9b913f854bf60af5300e9411b6787
Northcoders4 <northcoders4@Northcoders4s-MacBook-Pro.local>
2019-05-09 09:26:16 +0100

    dev data added

--- db/data/dev-data/users.js
+++ db/data/dev-data/users.js
@@ -2,49 +2,49 @@ module.exports = [
   {
     email: 'jonny.bravo@arup.com',
     first_name: 'jonny',
-    lastName: 'bravo',
+    last_name: 'bravo',
     password: 'jonny123'
   },
   {
     email: 'tish.richardson@arup.com',
     first_name: 'tish',
-    lastName: 'richardson',
+    last_name: 'richardson',
     password: 'tish123'
   },
   {
     email: 'ben.adey@arup.com',
     first_name: 'ben',
-    lastName: 'adey-johnson',
+    last_name: 'adey-johnson',
     password: 'ben123'
   },
   {
     email: 'martyn.tattersal@arup.com',
     first_name: 'martyn',
-    lastName: 'tattersal',
+    last_name: 'tattersal',
     password: 'martyn123'
   },
   {
     email: 'jim.bob@arup.com',
     first_name: 'jim',
-    lastName: 'bob',
+    last_name: 'bob',
     password: 'jim123'
   },
   {
     email: 'james.smith@arup.com',
     first_name: 'james',
-    lastName: 'smith',
+    last_name: 'smith',
     password: 'james123'
   },
   {
     email: 'anne.smith@arup.com',
     first_name: 'anne',
-    lastName: 'smith',
+    last_name: 'smith',
     password: 'anne123'
   },
   {
     email: 'jack.white@arup.com',
     first_name: 'jack',
-    lastName: 'white',
+    last_name: 'white',
     password: 'jack123'
   }
 ];
