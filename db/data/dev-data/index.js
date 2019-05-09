b58248dc1530779baf3468c7ff1f91fab7c209e6
Northcoders4 <northcoders4@Northcoders4s-MacBook-Pro.local>
2019-05-09 09:27:46 +0100

    fully seeded

--- db/data/dev-data/index.js
+++ db/data/dev-data/index.js
@@ -1,8 +1,8 @@
 exports.bookletData = require('./booklet');
 exports.jobsData = require('./jobs');
 exports.mapsData = require('./maps');
-exports.sitesData = require('./sites');
 exports.usersData = require('./users');
+exports.sitesData = require('./sites');
 exports.siteUsersData = require('./site_users');
 exports.questionsData = require('./questions');
 exports.riskLevelData = require('./risk_level');
